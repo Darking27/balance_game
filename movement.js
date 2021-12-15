bc58f8d5fb98811c8e238a70f8db4118b5838963
Felix Rohn <felix.rohn@student.kit.edu>
2021-12-15 17:04:37 +0100

    Added levels to game

--- movement.js
+++ movement.js
@@ -16,13 +16,6 @@ function deviceOrientationHandler (eventData) {
 const CVS = document.getElementById("game");
 const CTX = CVS.getContext("2d");
 
-var started = null;
-window.addEventListener('click', () => {
-  if (started) return;
-  started = true;
-  landscape_fullscreen();
-})
-
 document.getElementById('container').addEventListener('fullscreenchange', (event) => {
     // document.fullscreenElement will point to the element that
     // is in fullscreen mode if there is one. If not, the value
@@ -31,7 +24,6 @@ document.getElementById('container').addEventListener('fullscreenchange', (event
       console.log(`Element: ${document.fullscreenElement.id} entered fullscreen mode.`);
     } else {
       console.log('Leaving full-screen mode.');
-      started = null;
       CVS.style.display = "none";
       clearInterval(interval);
     }
@@ -76,6 +68,8 @@ function initialize_context() {
     console.log("Target X: " + targetX);
     console.log("Target Y: " + targetY);
 
+    var level = 1;
+
     function loop() {
         // calculation of movement
         var factor = 0.01;
@@ -94,8 +88,6 @@ function initialize_context() {
             speedY = -3;
         }
 
-        console.log("Speed Y: " + speedY + "Speed X: " + speedX);
-
         positionX = positionX + speedX;
         positionY = positionY + speedY;
 
@@ -109,6 +101,7 @@ function initialize_context() {
         CTX.fillStyle = "gray";
         CTX.fillRect(50, 50, W - 100, H - 100);
 
+
         CTX.fillStyle = "blue";
         CTX.fillRect((targetX - 10) + (W / 2), (targetY - 10) + (H / 2), 20, 20);
 
@@ -116,6 +109,10 @@ function initialize_context() {
         CTX.fillStyle = "red";
         CTX.arc(positionX + (W / 2), positionY + (H / 2), 10, 0, 2 * Math.PI);
         CTX.fill();
+
+        CTX.fillStyle = "black";
+        CTX.font = "30px Arial";
+        CTX.fillText("Level: " + level, 20, 30);
 
         var diffX = Math.abs(targetX - positionX);
         var diffY = Math.abs(targetY - positionY);
@@ -126,4 +123,100 @@ function initialize_context() {
         }
 
     }
+}
+
+function start_game() {
+
+    // enter fullscreen mode
+    var game_display = document.querySelector("#container");
+    if(game_display.requestFullscreen) {
+        game_display.requestFullscreen();
+    } else if(game_display.webkitRequestFullScreen) {
+        game_display.webkitRequestFullScreen();
+    }
+
+    // set lanscape mode
+    screen.orientation.lock("landscape-primary");
+
+    var level = 1;
+
+    window.setTimeout(load_new_Level, 500);
+
+    function load_new_Level() {
+        CVS.style.display = "block";
+
+        var W = CVS.width = screen.width;
+        var H = CVS.height = screen.height;
+
+        var maxX = (W - 100) / 2;
+        var maxY = (H - 100) / 2;
+
+        var positionX = 0;
+        var positionY = 0;
+
+        var speedX = 0;
+        var speedY = 0;
+
+        var targetX = Math.random() * (2 * maxX) - maxX;
+        var targetY = Math.random() * (2 * maxY) - maxY;
+
+        interval = window.setInterval(loop, 15);
+
+        function loop() {
+            // calculation of movement
+            var factor = 0.01 + (0.001 * level);
+    
+            speedX = speedX + (tiltFB * factor);
+            speedY = speedY - (tiltLR * factor);
+    
+            if (speedX > 3) {
+                speedX = 3;
+            } else if (speedX < -3) {
+                speedX = -3;
+            }
+            if (speedY > 3) {
+                speedY = 3;
+            } else if (speedY < -3) {
+                speedY = -3;
+            }
+    
+            positionX = positionX + speedX;
+            positionY = positionY + speedY;
+    
+            if (Math.abs(positionX) > maxX || Math.abs(positionY) > maxY) {
+                alert("Lose");
+            }
+    
+            CTX.fillStyle = "white";
+            CTX.fillRect(0, 0, W, H);
+    
+            CTX.fillStyle = "gray";
+            CTX.fillRect(50, 50, W - 100, H - 100);
+    
+    
+            CTX.fillStyle = "blue";
+            CTX.fillRect((targetX - 10) + (W / 2), (targetY - 10) + (H / 2), 20, 20);
+    
+            CTX.beginPath();
+            CTX.fillStyle = "red";
+            CTX.arc(positionX + (W / 2), positionY + (H / 2), 10, 0, 2 * Math.PI);
+            CTX.fill();
+    
+            CTX.fillStyle = "black";
+            CTX.font = "30px Arial";
+            CTX.fillText("Level: " + level, 20, 30);
+    
+            var diffX = Math.abs(targetX - positionX);
+            var diffY = Math.abs(targetY - positionY);
+    
+            var distance = Math.sqrt((diffX * diffX) + (diffY * diffY));
+            if (distance < 5 && speedX < 0.5 && speedY < 0.5) {
+                window.clearInterval(interval);
+                level = level + 1;
+                load_new_Level();
+            }
+    
+        }
+    }
+
 }
